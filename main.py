1632087986ece135e101e6d8e4bcb0c1e9ee39e8
diego192599 <di.cramire@udeo.edu.gt>
2025-07-21 11:07:31 -0600

    commit1 factorial y creacion de menu

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Menu")
+from tkinter.constants import SEL_FIRST
+
+
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        print(n)
+        return n*factorial(n-1)
+
+ def menu():
+     opcion=0
+     while True:
+         print("===Menu===")
+         print("1. Factorial")
+         print("2. Suma de primeros numeros")
+         print("3. Calcular el fibonacci")
+         print("4. Contar letras")
+         print("5. Invertir una cadena de texto")
+         print("6. Calcular potencia")
+         print("7. Salir")
+         opcion=int(input("Selecciona una opcion"))
+         if opcion==1:
+             n=int(input("Ingrese un numero entero que desee saber su factorial: "))
+             print(factorial(n))
+         elif opcion==2:
